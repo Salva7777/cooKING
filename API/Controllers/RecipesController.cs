ee79a0ebfe45194cd2fe886dd35a5f76b5c985ae
Salvador Estrada <salvadorestrada02@hotmail.com>
2022-04-22 15:44:43 +0100

    V0.2 'Recipe Controller Complete'

--- API/Controllers/RecipesController.cs
+++ API/Controllers/RecipesController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Recipes;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -10,22 +12,36 @@ namespace API.Controllers
 {
     public class RecipesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public RecipesController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Recipe>>> GetRecipes()
         {
-            return await _context.Recipes.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
-        [HttpGet("id")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<Recipe>> GetRecipe(Guid id)
         {
-            return await _context.Recipes.FindAsync(id);
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRecipe(Recipe recipe)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Recipe = recipe }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditRecipe(Guid id, Recipe recipe)
+        {
+            recipe.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Recipe = recipe }));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipe(Guid id)
+        {
+            
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Core;
+using Application.Recipes;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -38,6 +41,9 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            //adicionar o cors para ligar o client à API
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
